4689206369d1f154cc9d02e52e5d2d7998471e13
vikingrag <100153634+vikingrag@users.noreply.github.com>
2022-06-25 13:21:38 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,94 @@
+startBtn = document.querySelector
+	('#start')
+const screens = document.querySelectorAll
+	('.screen')
+const timeList = document.querySelector
+	('#time-list')
+const timeEl = document.querySelector
+	('#time')
+const board = document.querySelector
+	('#board')
+const colors = ['#E9EC45', '#74EC45', '#52E9CB', '#1D1AF4', '#F41AD7', '#1AE9F4', '#F4F01A']
+let time = 0
+let Score = 0
+
+
+startBtn.addEventListener('click', (event) => {
+	event.preventDefault()
+	screens[0].classList.add('up')
+})
+
+timeList.addEventListener('click', event => {
+	if (event.target.classList.contains
+		('time-btn')) {
+		time = parseInt(event.target.getAttribute
+			('data-time'))
+		screens[1].classList.add('up')
+		startGame()
+	}
+})
+
+board.addEventListener('click', event => {
+	if (event.target.classList.contains('circle')) {
+		Score++
+		event.target.remove()
+		createRandomCircle()
+	}
+})
+
+
+
+function startGame() {
+	setInterval(decreaceTime, 1000)
+	createRandomCircle()
+	setTime(time)
+}
+
+function decreaceTime() {
+	if (time === 0) {
+		finnishGame()
+	} else {
+		let current = --time
+		if (current < 10) {
+			current = `0${current}`
+		}
+		setTime(current)
+	}
+}
+
+function setTime(value) {
+	timeEl.innerHTML = `00:${value}`
+}
+
+function finnishGame() {
+	timeEl.parentNode.classList.add('hide')
+	board.innerHTML = `<h1>счет: <span class="primary"> ${Score}</span></span></h1>`
+}
+
+function createRandomCircle() {
+	const circle = document.createElement('div')
+	const size = getRandomNumber(10, 60)
+	const { width, height } = board.
+		getBoundingClientRect()
+	const x = getRandomNumber(0, width - size)
+	const y = getRandomNumber(0, height - size)
+	const colors = getRandomColor()
+
+	circle.style.background = colors;
+
+	circle.classList.add('circle')
+	circle.style.width = `${size}px`
+	circle.style.height = `${size}px`
+	circle.style.top = `${y}px`
+	circle.style.left = `${x}px`
+
+	board.append(circle)
+}
+
+function getRandomNumber(min, max) {
+	return Math.round(Math.random() * (max - min) + min)
+}
+
+function getRandomColor() {
+	return colors[Math.floor(Math.random() * colors.length)]
+}
